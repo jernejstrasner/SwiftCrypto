c00a115c0e79eb9b68a53060670469a0a9a349f4
Jernej Strasner <jernej.strasner@gmail.com>
2018-03-24 15:42:45 -0700

    Updated dependency version

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/jernejstrasner/CCommonCrypto.git", .branch("master"))
+        .package(url: "https://github.com/jernejstrasner/CCommonCrypto.git", from: "1.1.0")
     ],
     targets: [
         .target(
